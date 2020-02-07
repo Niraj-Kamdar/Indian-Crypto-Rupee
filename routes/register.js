eff92d18a46a98e8e847fc181c6b70ec9493c5aa
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-02-08 02:33:31 +0530

    store encoded account on database

--- routes/register.js
+++ routes/register.js
@@ -1,18 +1,24 @@
 const router = require("express").Router();
 const User = require("../models/user");
-
+const Web3 = require("web3");
+const web3 = new Web3("HTTP://127.0.0.1:7545");
 router.get("/", (req, res) => ***REMOVED***
   res.render("register", ***REMOVED*** error: null ***REMOVED***);
 ***REMOVED***);
 
 router.post("/", (req, res) => ***REMOVED***
   const ***REMOVED*** name, email, username, password, mobile ***REMOVED*** = req.body;
+  const acc = web3.eth.accounts.create();
+  const data = JSON.stringify(
+    web3.eth.accounts.encrypt(acc.privateKey, username + password)
+  );
   const user = new User(***REMOVED***
     name,
     email,
     username,
     password,
-    mobile
+    mobile,
+    data
 ***REMOVED***);
   user
     .save()
---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+module.exports = ***REMOVED***
+  networks: ***REMOVED***
+    development: ***REMOVED***
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 5000000000 // Match any network id
+  ***REMOVED***
+***REMOVED***
+***REMOVED***;
