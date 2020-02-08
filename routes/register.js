91791616f529ec8e6a02862a5ca7848a18fb4cfa
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-02-08 10:21:33 +0530

    add public and private key fields

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const User = require("../models/user");
-<<<<<<< HEAD
 
 const checkUser = (req, res, next) => ***REMOVED***
   const ***REMOVED*** UserToken ***REMOVED*** = req.cookies;
@@ -18,19 +17,21 @@ const checkUser = (req, res, next) => ***REMOVED***
 ***REMOVED***;
 
 router.get("/", checkUser, (req, res) => ***REMOVED***
-=======
-const Web3 = require("web3");
-const web3 = new Web3("HTTP://127.0.0.1:7545");
-router.get("/", (req, res) => ***REMOVED***
->>>>>>> 3ca04edd0e1f510960326fa4b7d6e770c8cbf3bf
   res.render("register", ***REMOVED*** error: null ***REMOVED***);
 ***REMOVED***);
 
 router.post("/", checkUser, (req, res) => ***REMOVED***
-  const ***REMOVED*** name, email, username, password, mobile ***REMOVED*** = req.body;
-  const acc = web3.eth.accounts.create();
+  const ***REMOVED***
+    name,
+    email,
+    username,
+    password,
+    mobile,
+    publicKey,
+    privateKey
+***REMOVED*** = req.body;
   const data = JSON.stringify(
-    web3.eth.accounts.encrypt(acc.privateKey, username + password)
+    web3.eth.accounts.encrypt(privateKey, username + password)
   );
   const user = new User(***REMOVED***
     name,
@@ -38,12 +39,15 @@ router.post("/", checkUser, (req, res) => ***REMOVED***
     username,
     password,
     mobile,
-    data
+    data,
+    publicKey
 ***REMOVED***);
+
   user
     .save()
     .then(docs => ***REMOVED***
       res.cookie(`UserToken`, docs._id);
+
       return res.redirect("/dashboard");
   ***REMOVED***)
     .catch(err => ***REMOVED***
